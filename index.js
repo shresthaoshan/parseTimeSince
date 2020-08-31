f30f94d7a2ea856184a009f1642e7aedb678575a
shresthaoshan <os02411418@student.ku.edu.np>
2020-08-31 08:40:11 +0545

    checks added

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 function parseTimeSince(parseSince) {
+    if (typeof parseSince == 'object') {
+        console.error('Parameter should not be an object but an integer.')
+        return null
+    }
+    if (!parseInt(parseSince)) {
+        console.error("Could not parse the parameter into an integer.")
+        return null
+    }
+    if (new Date(parseInt(parseSince)) == 'Invalid Date') {
+        console.error("Parameter should be a number or a string that can be parsed into date.")
+        return null
+    }
     let delta = Math.floor((Date.now()-parseSince)/(1000*60))
     let timeElapsed = ""
     if (delta == 0) return 'Just now'
