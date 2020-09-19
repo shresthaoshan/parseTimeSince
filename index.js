696fd34a557832156455f4f92975cf8a56882f35
shresthaoshan <os02411418@student.ku.edu.np>
2020-09-19 12:18:53 +0545

    some restructering

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function parseTimeSince(parseSince) {
+module.exports = parseSince => {
     if (typeof parseSince == 'object') {
         console.error('Parameter should not be an object but an integer.')
         return null
@@ -11,33 +11,5 @@ function parseTimeSince(parseSince) {
         console.error("Parameter should be a number or a string that can be parsed into date.")
         return null
     }
-    let delta = Math.floor((Date.now()-parseSince)/(1000*60))
-    let timeElapsed = ""
-    if (delta == 0) return 'Just now'
-    else {
-        if (delta == 1) timeElapsed = "minute"
-        else if (delta < 60) timeElapsed = "minutes"
-        else {
-            delta /= 60
-            if (Math.floor(delta) == 1) timeElapsed = 'hour'
-            else if (delta < 24) timeElapsed = 'hours'
-            else {
-                delta /= 24
-                if (Math.floor(delta) == 1) timeElapsed = 'day'
-                else if (delta < 30) timeElapsed = 'days'
-                else {
-                    delta /= 30
-                    if (Math.floor(delta) == 1) timeElapsed = 'month'
-                    else if (delta < 12) timeElapsed = 'months'
-                    else {
-                        delta /= 12
-                        if (Math.floor(delta) == 1) timeElapsed = 'year'
-                        else timeElapsed = 'years'
-                    }
-                }
-            }
-        }
-    }
-    return `${Math.floor(delta)} ${timeElapsed} ago.`
+    return (new (require('./services/ParseElapsed'))).compute(parseSince)
 }
-module.exports = parseTimeSince
